d6ed81362a37a067495045fa989b74751f6a0e1c
Kyleshao1 <Kyleshao@sina.com>
2025-10-25 18:19:11 +0800

    edit vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
@@ -9,9 +9,7 @@ export default defineConfig(({ mode }) => ({
       '/api': 'https://kyleshao-forum-backend.onrender.com'
     }
   },
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(
-      process.env.VITE_API_URL || 'https://kyleshao-forum-backend.onrender.com'
-    )
+  build: {
+    outDir: 'dist'
   }
-}));
+});
