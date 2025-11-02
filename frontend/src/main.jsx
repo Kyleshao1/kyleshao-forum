7bfaf1e16571fc9c90aab9c8f0da2b84a66b5caa
Kyleshao1 <Kyleshao@sina.com>
2025-11-02 12:14:58 +0800

    edit main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 console.log('App loaded');
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
